81771b6a26e0d047a36418cee78bb4479ae34122
ViniciusSantana95 <Kvinin8@gmail.com>
2022-05-03 22:21:59 +0000

    alteracao alter table

--- V01_01__create_table_consumidor.sql
+++ V01_01__create_table_consumidor.sql
@@ -1,6 +1,6 @@
 create table Consumidor(
     id         int           NOT NULL,
     nome       varchar(50)   NOT NULL,
-    endereco_fk id           NOT NULL,
+    endereco_fk int           NOT NULL,
     constraint pk_consumidor primary key (id)
 );
--- V01_11__create_funcionario_medidor_fk.sql
+++ V01_11__create_funcionario_medidor_fk.sql
@@ -1,4 +1,4 @@
-ALTER TABLE
+ALTER TABLE funcionario
     ADD CONSTRAINT funcionario_medidor_fk
         FOREIGN KEY (medidor_fk)
             REFERENCES medidor (id);
